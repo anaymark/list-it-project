9c63a2bb3e2a5cb884aa959dcd4e7a448118766d
dangerzone08 <alex.naymark18@gmail.com>
2018-10-31 17:31:28 -0600

    add items spec

--- src/components/_test_/ItemList.test.js
+++ src/components/_test_/ItemList.test.js
@@ -1,5 +1,40 @@
 import React from 'react';
 import { shallow } from '../../enzyme';
-import { mount } from '../../enzyme';
-import ItemList from '../ShopList';
+import ItemList from '../ItemList';
 import jasmineEnzyme from 'jasmine-enzyme';
+
+
+describe('Item', () => {
+    let database;
+    beforeEach(() => {
+       database = {
+           database: () => {
+              return   {
+                ref: () => {
+                    return {
+                    on: () => {}
+                    }
+                }
+              }
+            } 
+           }
+    },
+    () => {
+        jasmineEnzyme();
+    }
+    )
+
+    it('renders messages', () => {
+        const list = 'one';
+        const user = 'Mark'
+        const wrapper = shallow(<ItemList db = {database} newListName = {list} user = {user}/>)
+        expect(wrapper.find('.all-items')).toBeDefined();
+    })
+
+    it('Items list for christmas list', () => {
+        const newListName = "christmas";
+        const wrapper = shallow(<ItemList db = {database} newListName = {newListName}/>)
+        expect(wrapper.find('.this.props.activeList')).toBeTruthy();
+    })
+
+})
--- src/components/_test_/ShopList.test.js
+++ src/components/_test_/ShopList.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from '../../enzyme';
-import { mount } from '../../enzyme';
 import ShopList from '../ShopList';
 import jasmineEnzyme from 'jasmine-enzyme';
 
