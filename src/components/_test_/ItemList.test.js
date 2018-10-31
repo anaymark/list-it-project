4f2e93caaff3a60199a0f1f08ac68f76c8c87de6
dangerzone08 <alex.naymark18@gmail.com>
2018-10-31 15:30:28 -0600

    add specs

---
+++ src/components/_test_/ItemList.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { shallow } from '../../enzyme';
+import { mount } from '../../enzyme';
+import ItemList from '../ShopList';
+import jasmineEnzyme from 'jasmine-enzyme';
---
+++ src/components/_test_/ShopList.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from '../../enzyme';
+import { mount } from '../../enzyme';
+import ShopList from '../ShopList';
+import jasmineEnzyme from 'jasmine-enzyme';
+
+
+
+describe('List Test', () => {
+    let database;
+    beforeEach(() => {
+       database = {
+           database: () => {
+              return   {
+                ref: () => {
+                    return {
+                    on: () => {}
+                    }
+                }
+              }
+            } 
+           }
+    },
+    () => {
+        jasmineEnzyme();
+    }
+    )
+
+    it('renders list', () => {
+        const list = 'one';
+        const wrapper = shallow(<ShopList db = {database} newListName = {list}/>)
+        expect(wrapper.find('.all-lists')).toBeDefined();
+    })
+
+    it('add a list/list exists', () => {
+        const newListName = "christmas";
+        const wrapper = shallow(<ShopList db = {database} newListName = {newListName}/>)
+        expect(wrapper.find('.this.props.activeList')).toBeTruthy();
+    })
+
+})
